9ee8fae39da7e6fee33f51a9d454ee61d0967c4f
errollw <errollw@gmail.com>
2014-01-16 21:30:14 +0000

    Updated bezier, removed book image

--- js/ps.compgraph.bezier.js
+++ js/ps.compgraph.bezier.js
@@ -1,36 +1,57 @@
 var width = view.size.width,
 	height = view.size.height;
 
+var red = '#e4141b';
+
+// initialize starting Bezier cubic
 var vector = new Point({ angle: 0, length: width / 3 });
 var pad = width / 30;
-var handleTexts = [];
+var bezier = new Path({
+	segments    : [ [[pad*2, height-pad*2], null, vector.rotate(-70)*1.5],
+	                [[width - pad*2, height / 2], vector.rotate(180+45), null] ],
+	strokeColor : red
+});
 
-var path = new Path();
-path.segments = [ [[pad*2, height-pad*2], null, vector.rotate(-70)*1.5],
-	              [[width - pad*2, height / 2], vector.rotate(180+45), null]
-];
-path.fullySelected = true;
-var curve = path.firstCurve;
+var curve = bezier.firstCurve;
 
-var dragged_cp;
-var dragging;
+// dashed lines between P0-P1 and P2-P3
+var cp_line_1 = new Path.Line({ strokeColor: red });
+var cp_line_2 = new Path.Line({ strokeColor: red });
+cp_line_1.dashArray = cp_line_2.dashArray = [2,4];
 
-var cps,
-	labels = {'P0': new PointText({ content: 'P0', justification: 'center' }),
-		      'P1': new PointText({ content: 'P1', justification: 'center' }),
-		      'P2': new PointText({ content: 'P2', justification: 'center' }),
-		      'P3': new PointText({ content: 'P3', justification: 'center' })}
+// solid circles at each CP
+var cp_circ_0 = new Path.Circle({ radius: 2, fillColor: red }),
+	cp_circ_1 = new Path.Circle({ radius: 2, fillColor: red }),
+	cp_circ_2 = new Path.Circle({ radius: 2, fillColor: red }),
+	cp_circ_3 = new Path.Circle({ radius: 2, fillColor: red });
 
-function update_cps(){
+var dragged_cp;	// which CP is currently being dragged
+var dragging;	// whether dragging is currently happening
+
+var cps 		// a dict of all CPs (updated on move)
+
+function update_cps(point){
 
 	cps = { 'P0': curve.point1,
 	        'P1': curve.point1 + curve.handle1,
 	        'P2': curve.point2 + curve.handle2,
 	        'P3': curve.point2 };
 
-	for (cp in cps){
-		labels[cp].point = cps[cp] - new Point(0,8);
+	// moved currently dragged point
+	switch (dragged_cp){
+		case 'P0' : curve.handle1 += (curve.point1-point);
+					curve.point1  = point;                break;
+		case 'P1' : curve.handle1 = point - curve.point1; break;
+		case 'P2' : curve.handle2 = point - curve.point2; break;
+		case 'P3' : curve.handle2 += (curve.point2-point);
+					curve.point2  = point;                break;
 	}
+
+	// update curve and line positions
+	cp_circ_0.position = cp_line_1.firstCurve.point1 = curve.point1;
+	cp_circ_1.position = cp_line_1.firstCurve.point2 = curve.point1 + curve.handle1;
+	cp_circ_2.position = cp_line_2.firstCurve.point1 = curve.point2 + curve.handle2;
+	cp_circ_3.position = cp_line_2.firstCurve.point2 = curve.point2;
 }
 
 function onMouseDown(event) {
@@ -40,7 +61,6 @@ function onMouseDown(event) {
 	if (!hit_result) return;
 
 	for (cp in cps){
-		console.log((cps[cp]-event.point).length)
 		if ( (cps[cp]-event.point).length < hit_options.tolerance){
 			dragged_cp = cp;
 			dragging = true;
@@ -50,9 +70,7 @@ function onMouseDown(event) {
 
 function onMouseUp() {
 	dragging = false;
-	update_cps();
 }
-
 
 function onMouseMove(event) {
 
@@ -65,41 +83,8 @@ function onMouseMove(event) {
 	if (point.y > view.size.height - pad) point.y = view.size.height - pad;
 	if (point.x > view.size.width - pad) point.x = view.size.width - pad;
 
-	// moved currently dragged point
-	switch (dragged_cp){
-		case 'P0' : curve.handle1 += (curve.point1-point);
-					curve.point1  = point;                break;
-		case 'P1' : curve.handle1 = point - curve.point1; break;
-		case 'P2' : curve.handle2 = point - curve.point2; break;
-		case 'P3' : curve.handle2 += (curve.point2-point);
-					curve.point2  = point;                break;
-	}
-
-	update_cps();
-}
-
-project.currentStyle.fillColor = 'black';
-
-var segment = path.firstSegment;
-var text = new PointText({
-	point: segment.point - [0, 10],
-	content: i,
-	justification: 'center'
-});
-
-for (var i = 0; i < 2; i++) {
-	handleTexts.push(
-		new PointText({
-			content: 'handleIn',
-			justification: 'center'
-		}),
-		new PointText({
-			content: 'handleOut',
-			justification: 'center'
-		})
-	);
+	update_cps(point);
 }
 
 update_cps();
-onMouseMove({ point: view.center - vector.rotate(-90) });
 
